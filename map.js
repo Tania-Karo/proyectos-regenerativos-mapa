60a698b33bd8006c39ae444b7ccc3410110aa04c
Tania-Karo <karotania25@gmail.com>
2025-08-01 18:02:03 -0300

    mapa en proceso (no anda ahora)

--- map.js
+++ map.js
@@ -1,10 +1,32 @@
 // Centrar el mapa en Boquerón, Paraguay (lat, lng)
 var map = L.map('map').setView([-21.5821, -60.7924], 8);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles © Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
+
+// create the sidebar instance and add it to the map
+var sidebar = L.control.sidebar({ container: 'sidebar' })
+    .addTo(map)
+    .open('home');
+
+// add panels dynamically to the sidebar
+sidebar
+    .addPanel({
+        id:   'js-api',
+        tab:  '<i class="fa fa-gear"></i>',
+        title: 'JS API',
+        pane: '<p>The Javascript API allows to dynamically create or modify the panel state.<p/><p><button onclick="sidebar.enablePanel(\'mail\')">enable mails panel</button><button onclick="sidebar.disablePanel(\'mail\')">disable mails panel</button></p><p><button onclick="addUser()">add user</button></b>',
+    })
+    // add a tab with a click callback, initially disabled
+    .addPanel({
+        id:   'mail',
+        tab:  '<i class="fa fa-envelope"></i>',
+        title: 'Messages',
+        button: function() { alert('opened via JS callback') },
+        disabled: true,
+    })
+
 
 
 
